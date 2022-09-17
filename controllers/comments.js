13cfc3b8af931e6cc9f793ec298a760f18585d7e
unknown <johnzak80@gmail.com>
2022-09-16 20:37:49 -0400

    comments!

---
+++ controllers/comments.js
@@ -0,0 +1,33 @@
+const Comment = require("../models/Comment");
+
+module.exports = {
+ 
+  createComment: async (req, res) => {
+    console.log(req)
+    try {
+    await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+        user: req.user.id
+      });
+      console.log("comment has been added!");
+      res.redirect("/post/"+req.params.id);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+//   deleteComment: async (req, res) => {
+//     try {
+//       // Find comment by id
+//       let comment= await Comment.findById({ _id: req.params.id });
+//       // Delete post from db
+//       await Comment.remove({ _id: req.params.id });
+//       console.log("Deleted Comment");
+//       res.redirect("/post/"+req.params.id);
+//     } catch (err) {
+//       res.redirect("/profile");
+//     }
+//   },
+}
